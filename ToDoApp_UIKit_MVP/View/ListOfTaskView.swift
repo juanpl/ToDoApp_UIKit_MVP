41048b4d572650fb5125f6133c3eb6258b193a4c
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-11-28 13:45:51 -0500

    The Text Field, create button and collection was created

--- ToDoApp_UIKit_MVP/View/ListOfTaskView.swift
+++ ToDoApp_UIKit_MVP/View/ListOfTaskView.swift
@@ -8,12 +8,74 @@
 import UIKit
 
 class ListOfTaskView: UIViewController {
+    
+    var presenter = TaskPresenter()
+    
+    private let taskTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .systemFont(ofSize: 18, weight: .regular)
+        textView.backgroundColor = .systemGray6
+        textView.textColor = .label
+        textView.layer.cornerRadius = 12
+        textView.layer.borderColor = UIColor.systemGray3.cgColor
+        textView.layer.borderWidth = 1
+        textView.textContainerInset = .init(top: 8, left: 8, bottom: 8, right: 8)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    private lazy var createTaskButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Create", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 12
+        button.addTarget(self, action: #selector(didTapOnCreateTask), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var taskCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = .init(width: 340, height: 80)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemGroupedBackground
+        return collectionView
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        [taskTextView, createTaskButton, taskCollectionView].forEach(view.addSubview)
+        
+        NSLayoutConstraint.activate([
+            taskTextView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
+            taskTextView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            taskTextView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            taskTextView.heightAnchor.constraint(equalToConstant: 60),
+            
+            
+            createTaskButton.topAnchor.constraint(equalTo: taskTextView.bottomAnchor, constant: 6),
+            createTaskButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            createTaskButton.heightAnchor.constraint(equalToConstant: 40),
+            createTaskButton.widthAnchor.constraint(equalToConstant: 80),
+            
+            taskCollectionView.topAnchor.constraint(equalTo: createTaskButton.bottomAnchor, constant: 12),
+            taskCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            taskCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            taskCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
-
+    
+    @objc
+    func didTapOnCreateTask() {
+        print("Create task")
+        presenter.create(task: taskTextView.text)
+    }
     
 }
 
